649d728e41edb325c0cab10aa71e9a9876fd0c3d
Daryn <batyrerlik@gmail.com>
2023-06-14 12:26:29 +0600

    разработал ui компонент

---
+++ src/Components/UI/Form/FormElement.tsx
@@ -0,0 +1,46 @@
+import { TextField, Grid } from '@mui/material';
+import React, {ChangeEventHandler, HTMLInputTypeAttribute} from 'react';
+
+interface Props {
+  value: string;
+  label: string;
+  name: string;
+  onChange: ChangeEventHandler<HTMLInputElement>;
+  type?: HTMLInputTypeAttribute | 'select';
+  required?: boolean;
+  error?: string;
+  id?: string;
+}
+
+const FormElement = (
+  {
+    name,
+    label,
+    value,
+    onChange,
+    type = 'text',
+    required = false,
+    error,
+    id,
+  }: Props
+) => {
+  return (
+    <Grid item xs={12}>
+      <TextField
+        id={id}
+        type={type}
+        required={required}
+        value={value}
+        onChange={onChange}
+        name={name}
+        variant='outlined'
+        fullWidth
+        label={label}
+        error={!!error}
+        helperText={error}
+      />
+    </Grid>
+  )
+}
+
+export default FormElement;
