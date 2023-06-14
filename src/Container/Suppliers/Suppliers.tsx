9cc614f0fcb29273ea2b82be79befb406a39e992
Daryn <batyrerlik@gmail.com>
2023-06-14 13:22:15 +0600

    доработал ссылки и подправил ключи для вывод

--- src/Container/Suppliers/Suppliers.tsx
+++ src/Container/Suppliers/Suppliers.tsx
@@ -1,34 +1,34 @@
-import React from "react";
-import {useGetAllSuppliersQuery} from "../../Store/services";
-import {Container, Grid} from "@mui/material";
-import CardSuppliers from "../../Components/Layout/Card/CardSuppliers";
-
-
+import React from 'react'
+import { useGetAllSuppliersQuery } from '../../Store/services/crmApi'
+import { Container } from '@mui/material'
+import CardSuppliers from '../../Components/Layout/Card/CardSuppliers'
 
 const handleClick = () => {
-    console.log('click')
+  console.log('click')
 }
 const Suppliers = () => {
-    const {data, isLoading} = useGetAllSuppliersQuery()
-    console.log(data)
-    if(isLoading) return <h1>Loading...</h1>
-    return (
-        <Container sx={{verticalAlign: 'center'}}>
-            {data.map((item: any) => {
-                return (
-                    <CardSuppliers
-                        name={item.name_good}
-                        image={item.image_large !== null ? item.image_large : 'image.jpeg'}
-                        create_date={item.create_date}
-                        description={item.goods_description}
-                        id_category={item.id_category}
-                        category_description={item.category_description}
-                        onClick={() => handleClick()}
-                        onClickDelete={() => handleClick()}/>
-                )
-            })}
-        </Container>
-    )
+  const { data, isLoading } = useGetAllSuppliersQuery()
+  console.log(data)
+  if (isLoading) return <h1>Loading...</h1>
+  return (
+    <Container sx={{ verticalAlign: 'center' }}>
+      {data.map((item: any) => {
+        return (
+          <CardSuppliers
+            key={item.id_supplier}
+            name={item.name_supplier}
+            image={item.image_large !== null ? item.image_large : 'image.jpeg'}
+            create_date={item.create_date}
+            description={item.goods_description}
+            id_category={item.id_category}
+            category_description={item.category_description}
+            onClick={() => handleClick()}
+            onClickDelete={() => handleClick()}
+          />
+        )
+      })}
+    </Container>
+  )
 }
 
 export default Suppliers
