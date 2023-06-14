1229f863740935cb14fd503624303d87b0765787
Daryn <batyrerlik@gmail.com>
2023-06-14 12:39:20 +0600

    добавлены роуты регистрации и авторизации

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,43 @@
-import React from 'react';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from './Components/Layout/Header/Header'
+import HomePage from './Container/HomePage/HomePage'
+import Items from './Container/Items/Items'
+import Suppliers from './Container/Suppliers/Suppliers'
+import Register from './Container/Register/Register'
+import Login from './Container/Login/Login'
+import ProtectedRoute from './Components/ProtectedRoute/ProtectedRoute'
+import { useAppSelector } from './Store/hooks'
 
-import Header from "./Components/Layout/Header/Header";
-import HomePage from "./Container/HomePage/HomePage";
-import Items from "./Container/Items/Items";
-import Suppliers from "./Container/Suppliers/Suppliers";
 function App() {
+  const { user } = useAppSelector((state) => state.auth)
   return (
-      <BrowserRouter>
-          <Routes>
-              <Route path={'/'} element={<Header/>}>
-                  <Route index element={<HomePage/>}/>
-                  <Route path={'/goods'} element={<Items/>}/>
-                  <Route path={'/suppliers'} element={<Suppliers/>}/>
-              </Route>
-          </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path={'/'} element={<Header />}>
+          <Route index element={<HomePage />} />
+          <Route path={'/goods'} element={<Items />} />
+          <Route path={'/suppliers'} element={<Suppliers />} />
+          <Route
+            path="/register"
+            element={
+              <ProtectedRoute isAllowed={!user} redirectPath="/">
+                <Register />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <ProtectedRoute isAllowed={!user} redirectPath="/">
+                <Login />
+              </ProtectedRoute>
+            }
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
-export default App;
+export default App
