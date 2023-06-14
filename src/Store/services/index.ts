301def6d64ef19aa508ce4617ec9c024ffb4404c
Daryn <batyrerlik@gmail.com>
2023-06-14 12:29:40 +0600

    разработан hook и эндпоинт регистрации и авторизаци

--- src/Store/services/index.ts
+++ src/Store/services/index.ts
@@ -1,30 +1,33 @@
-export * from './store'
-export * from './crmApi'
-
-
-// import {
-//     BaseQueryFn,
-//     createApi,
-//     FetchArgs,
-//     fetchBaseQuery,
-//     FetchBaseQueryError,
-//     FetchBaseQueryMeta
-// } from '@reduxjs/toolkit/query/react';
-// import {apiUrl} from "../../common/constans";
-// import {RootState} from "./store";
-// import {CustomError} from "../../interfaces/errors/CustomError";
-//
-// export const api = createApi({
-//     baseQuery: fetchBaseQuery({
-//         baseUrl: apiUrl,
-//         prepareHeaders: (headers, {getState}) => {
-//             const state: RootState = getState();
-//             const token = state.auth.user?.token;
-//             if (token) {
-//                 headers.set('Authorization', token);
-//             }
-//         }
-//     }) as BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError  | CustomError | {error: object}, {}, FetchBaseQueryMeta>,
-//     endpoints: () => ({}),
-//     tagTypes: ['Items', 'Suppliers'],
-// })
+import {
+    BaseQueryFn,
+    createApi,
+    FetchArgs,
+    fetchBaseQuery,
+    FetchBaseQueryError,
+    FetchBaseQueryMeta
+  } from "@reduxjs/toolkit/query/react";
+  import { apiUrl } from "../../common/constans";
+  import { RootState } from "../services/store";
+  import { CustomError } from "../../interfaces/errors/CustomError";
+  
+  export const api = createApi({
+    baseQuery: fetchBaseQuery({
+      baseUrl: apiUrl,
+      prepareHeaders: (headers, { getState }) => {
+        const state: RootState = getState();
+        const token = state.auth.user?.token;
+        if (token) {
+          headers.set("Authorization", token);
+        }
+      }
+    }) as BaseQueryFn<
+      string | FetchArgs,
+      unknown,
+      FetchBaseQueryError | CustomError | { error: object },
+      {},
+      FetchBaseQueryMeta
+    >,
+    endpoints: () => ({}),
+    // tagTypes: ["Items", "Suppliers"]
+    tagTypes: ["Items", "Suppliers"]
+});
