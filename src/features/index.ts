7428569b69c249d2bd0b6ff9f879e681c05eb41e
Daryn <batyrerlik@gmail.com>
2023-06-14 12:37:01 +0600

    разработан слайсл

---
+++ src/features/index.ts
@@ -0,0 +1,33 @@
+import {
+    BaseQueryFn,
+    createApi,
+    FetchArgs,
+    fetchBaseQuery,
+    FetchBaseQueryError,
+    FetchBaseQueryMeta
+  } from "@reduxjs/toolkit/query/react";
+  import { apiUrl } from "../common/constans";
+  import { RootState } from "../Store/services/store";
+  import { CustomError } from "../interfaces/errors/CustomError";
+  
+  export const api = createApi({
+    baseQuery: fetchBaseQuery({
+      baseUrl: apiUrl,
+      prepareHeaders: (headers, { getState }) => {
+        const state: RootState = getState();
+        const token = state.auth.user?.token;
+        if (token) {
+          headers.set("Authorization", token);
+        }
+      }
+    }) as BaseQueryFn<
+      string | FetchArgs,
+      unknown,
+      FetchBaseQueryError | CustomError | { error: object },
+      {},
+      FetchBaseQueryMeta
+    >,
+    endpoints: () => ({}),
+    // tagTypes: ["Items", "Suppliers"]
+    tagTypes: ["Items", "Suppliers"]
+});
