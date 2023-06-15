a55befaa32904cdf99ad3c3a00dc96682e383a22
Баилова Таншолпан <bailovat1@gmail.com>
2023-06-15 19:55:28 +0600

    создала интерфейсы для items, suppliers

---
+++ src/interfaces/Items.ts
@@ -0,0 +1,19 @@
+export interface Items {
+    id: number;
+    item_name: string;
+    item_description: string;
+    id_category: number;
+    image_small: string;
+    image_large: string;
+    id_user: number;
+    create_date: Date;
+};
+export interface Item {
+    item_name: string;
+    item_description: string;
+    id_category: number;
+    image_small: string;
+    image_large: string;
+    id_user: number;
+    create_date: Date;
+};
