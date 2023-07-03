ad4264467b23df4e16bd566347f163e95f261865
Баилова Таншолпан <bailovat1@gmail.com>
2023-07-03 13:43:03 +0600

    добавила rtk букетов

---
+++ src/Store/services/compositionBouquets.ts
@@ -0,0 +1,20 @@
+import {api} from '../../features/index';
+import { ICompositionBouquets, ICompositionBouquet } from '../../interfaces/ICompositionBouquets';
+
+const compositionBouquetsApi = api.injectEndpoints({
+    endpoints: (build) =>({
+        getCompositionBouquetById: build.query<ICompositionBouquets[], number | string>({
+            query: (id) => `/compositionBouquets/${id}`,
+        }),
+        createCompositionBouquet: build.mutation<ICompositionBouquets, ICompositionBouquet>({
+            query: (compositionBouquet) => ({
+              url: '/compositionBouquets',
+              method: 'POST',
+              body: compositionBouquet,
+            }),
+            invalidatesTags: ['CompositionBouquets'],
+        }),
+    }),
+});
+
+export const {useGetCompositionBouquetByIdQuery, useCreateCompositionBouquetMutation} = compositionBouquetsApi;
