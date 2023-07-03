9fd2dabd429882c165a36d32a74ce38f194938c9
Баилова Таншолпан <bailovat1@gmail.com>
2023-07-03 13:40:50 +0600

    добавила интерфейсы букетов

---
+++ src/interfaces/IBouquets.ts
@@ -0,0 +1,11 @@
+export interface IBouquet {
+    bouquet_name: string; 
+    bouquet_description: string;
+    author: string,
+    id_category: number | string;
+    image: string
+}
+
+export interface IBouquets extends IBouquet {
+    id: number;
+};
