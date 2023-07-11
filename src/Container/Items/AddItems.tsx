ee8e36e5bfe4e8e9cbcfebdb5619ed1b41ac01c9
Баилова Таншолпан <bailovat1@gmail.com>
2023-07-12 00:41:25 +0600

    изменила items

--- src/Container/Items/AddItems.tsx
+++ src/Container/Items/AddItems.tsx
@@ -1,8 +1,10 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
+import FileUpload from '../../Components/UI/Form/FileUpload';
 import FormElement from '../../Components/UI/Form/FormElement';
 import { CustomError } from '../../interfaces/errors/CustomError';
 import { useAddItemMutation } from '../../Store/services/items';
-import { useGetAllcategoriesQuery } from '../../Store/services/categories';
+import { useGetAllcategoriesQuery, useGetSubcategoriesByIdCategoryQuery } from '../../Store/services/categories';
+import { useGetUnderSubcategoriesByIdCategoryQuery } from '../../Store/services/subcategories';
 import BasicSelect from '../../Components/UI/Form/SelectFormElement';
 import { useAppSelector } from '../../Store/hooks';
 import { useNavigate } from 'react-router';
@@ -10,127 +12,178 @@ import { Container, Button, Snackbar, Alert } from '@mui/material';
 import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
 
 const AddItem = () => {
-	const [addItem, { error, isError }] = useAddItemMutation();
-	const { user } = useAppSelector((state) => state.auth);
+  const [addItem, { error, isError }] = useAddItemMutation();
+  const { user } = useAppSelector((state) => state.auth);
+  const { data: categories } = useGetAllcategoriesQuery();
 
-	const { data: categories } = useGetAllcategoriesQuery();
-	interface Props {
-		item_name: string;
-		item_description: string;
-		id_category: string;
-		id_subcategory: string;
-		id_under_subcategory: string;
-		id_user: number;
-	}
-	const initialFormState: Props = {
-		item_name: '',
-		item_description: '',
-		id_category: '',
-		id_subcategory: '',
-		id_under_subcategory: '',
-		id_user: user[0].id as number,
-	};
+  const [categoryId, setCategoryId] = useState(0);
+  const [subCategoryId, setSubCategoryId] = useState(0);
 
-	const [form, setForm] = useState<Props>(initialFormState);
-	const [open, setOpen] = useState(false);
+  const { data: subcategory } = useGetSubcategoriesByIdCategoryQuery(categoryId);
+  const { data: underSubcategory } = useGetUnderSubcategoriesByIdCategoryQuery(subCategoryId);
 
-	const navigate = useNavigate();
+  const [openItemId, setOpenItemId] = useState<number | null>(null);
 
-	const handleClose = () => {
-		setOpen(false);
-	};
+  const handleClick = (itemId: number) => {
+    setOpenItemId(itemId === openItemId ? null : itemId);
+    setCategoryId(itemId);
+  };
+  const handleClickSubcategory = (itemId: number) => {
+    setOpenItemId(itemId === openItemId ? null : itemId);
+    setSubCategoryId(itemId);
+  };
 
-	useEffect(() => {
-		setOpen(isError);
-	}, [isError]);
+  interface Props {
+    item_name: string;
+    item_description: string;
+    price: number;
+    id_category: string;
+    id_subcategory: string;
+    id_under_subcategory: string;
+    image_small: string;
+    id_user: number;
+  }
+  const initialFormState: Props = {
+    item_name: '',
+    item_description: '',
+    price: 0,
+    id_category: '',
+    id_subcategory: '',
+    id_under_subcategory: '',
+    image_small: '',
+    id_user: user[0].id as number,
+  };
 
-	const inputChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-		const { name, value } = e.target;
+  const [form, setForm] = useState<Props>(initialFormState);
+  const [open, setOpen] = useState(false);
 
-		setForm((prevState) => ({
-			...prevState,
-			[name]: value,
-		}));
-	};
+  const navigate = useNavigate();
 
-	const submitFormHandler = async (e: FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		const data = await addItem(form);
-		if (!(data as unknown as { error: object }).error) {
-			navigate('/items');
-			setForm(initialFormState);
-		}
-	};
-	const selectChangeHandler = (name: string, value: string) => {
-		setForm((prevState) => ({
-			...prevState,
-			[name]: value,
-		}));
-	};
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-	return (
-		<form onSubmit={submitFormHandler}>
-			<Container component="section" maxWidth="xs">
-				<Snackbar
-					anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-					open={open}
-					autoHideDuration={3000}
-					onClose={handleClose}
-				>
-					<Alert severity="error" onClose={handleClose}>
-						{(error as CustomError)?.data?.message}
-					</Alert>
-				</Snackbar>
-				<FormElement
-					value={form.item_name}
-					label="Товар"
-					name="item_name"
-					onChange={inputChangeHandler}
-				/>
-				<FormElement
-					value={form.item_description}
-					label="Описание"
-					name="item_description"
-					onChange={inputChangeHandler}
-				/>
-				<BasicSelect
-					value={form.id_category}
-					label="Категория"
-					name="id_category"
-					onChange={(value) => selectChangeHandler('id_category', value)}
-					options={
-						categories
-							? categories.map((category) => ({
-									id: category.id,
-									name: category.category_name,
-							  }))
-							: []
-					}
-				/>
-				<FormElement
-					value={form.id_subcategory}
-					label="Подкатегория"
-					name="id_subcategory"
-					onChange={inputChangeHandler}
-				/>
-				<FormElement
-					value={form.id_under_subcategory}
-					label="Подкатегория подкатегории"
-					name="id_under_subcategory"
-					onChange={inputChangeHandler}
-				/>
-				<Button
-					fullWidth
-					variant="contained"
-					color="success"
-					type="submit"
-					className="submit"
-					sx={{ marginBottom: 2, marginTop: 3 }}
-				>
-					Add
-				</Button>
-			</Container>
-		</form>
-	);
+  useEffect(() => {
+    setOpen(isError);
+  }, [isError]);
+
+  const inputChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    setForm((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const submitFormHandler = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const data = await addItem(form);
+    if (!(data as unknown as { error: object }).error) {
+      navigate('/items');
+      setForm(initialFormState);
+    }
+  };
+  const selectChangeHandler = (name: string, value: string) => {
+    setForm((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+  const fileChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      const file = e.target.files[0];
+      setForm((prevState) => ({
+        ...prevState,
+        image_small: file as unknown as string,
+      }));
+    }
+  };
+
+  return (
+    <form onSubmit={submitFormHandler}>
+      <Container component="section" maxWidth="xs">
+        <Snackbar
+          anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+          open={open}
+          autoHideDuration={3000}
+          onClose={handleClose}
+        >
+          <Alert severity="error" onClose={handleClose}>
+            {(error as CustomError)?.data?.message}
+          </Alert>
+        </Snackbar>
+        <FormElement value={form.item_name} label="Товар" name="item_name" onChange={inputChangeHandler} />
+        <FormElement
+          value={form.item_description}
+          label="Описание"
+          name="item_description"
+          onChange={inputChangeHandler}
+        />
+        <BasicSelect
+          value={form.id_category}
+          label="Категория"
+          name="id_category"
+          onChange={(value) => {
+            const categoryId = parseInt(value);
+            selectChangeHandler('id_category', value);
+            handleClick(categoryId);
+          }}
+          options={
+            categories
+              ? categories.map((category) => ({
+                  id: category.id,
+                  name: category.category_name,
+                }))
+              : []
+          }
+        />
+        <BasicSelect
+          value={form.id_subcategory}
+          label="Подкатегория"
+          name="id_subcategory"
+          onChange={(value) => {
+            const subCategoryId = parseInt(value);
+            selectChangeHandler('id_subcategory', value);
+            handleClickSubcategory(subCategoryId);
+          }}
+          options={
+            subcategory
+              ? subcategory.map((subcategory) => ({
+                  id: subcategory.id,
+                  name: subcategory.subcategory_name,
+                }))
+              : []
+          }
+        />
+        <BasicSelect
+          value={form.id_under_subcategory}
+          label="Подкатегория подкатегории"
+          name="id_under_subcategory"
+          onChange={(value) => selectChangeHandler('id_under_subcategory', value)}
+          options={
+            underSubcategory
+              ? underSubcategory.map((undersubs) => ({
+                  id: undersubs.id,
+                  name: undersubs.under_subcategory_name,
+                }))
+              : []
+          }
+        />
+
+        <FileUpload onChange={fileChangeHandler} name="image_small" label="Фото" />
+        <Button
+          fullWidth
+          variant="contained"
+          color="success"
+          type="submit"
+          className="submit"
+          sx={{ marginBottom: 2, marginTop: 3 }}
+        >
+          Add
+        </Button>
+      </Container>
+    </form>
+  );
 };
 export default AddItem;
--- src/Container/Items/EditItem.tsx
+++ src/Container/Items/EditItem.tsx
@@ -1,168 +1,192 @@
-/* eslint-disable @typescript-eslint/no-misused-promises */
-/* eslint-disable @typescript-eslint/await-thenable */
-/* eslint-disable @typescript-eslint/no-floating-promises */
 import FormElement from '../../Components/UI/Form/FormElement';
 import { CustomError } from '../../interfaces/errors/CustomError';
-import {
-	useEditItemMutation,
-	useGetItemByIdQuery,
-} from '../../Store/services/items';
-import { useGetAllcategoriesQuery } from '../../Store/services/categories';
+import { useEditItemMutation, useGetItemByIdQuery } from '../../Store/services/items';
+import { useGetAllcategoriesQuery, useGetSubcategoriesByIdCategoryQuery } from '../../Store/services/categories';
 import BasicSelect from '../../Components/UI/Form/SelectFormElement';
 import { useAppSelector } from '../../Store/hooks';
+import { useGetUnderSubcategoriesByIdCategoryQuery } from '../../Store/services/subcategories';
 import { useNavigate } from 'react-router';
 import { Container, Button, Snackbar, Alert } from '@mui/material';
 import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 export interface ItemProps {
-	item_name: string;
-	item_description: string;
-	id_category: string;
-	id_subcategory: string;
-	id_under_subcategory: string;
-	id_user: any;
+  item_name: string;
+  item_description: string;
+  id_category: string;
+  id_subcategory: string;
+  id_under_subcategory: string;
+  id_user: any;
 }
 
 const EditItem = () => {
-	const { id } = useParams();
-	const { data: itemById, refetch } = useGetItemByIdQuery(id as string, {
-		refetchOnMountOrArgChange: false,
-	});
+  const { id } = useParams();
+  const { data: itemById, refetch } = useGetItemByIdQuery(id as string, {
+    refetchOnMountOrArgChange: false,
+  });
 
-	const { data: categories } = useGetAllcategoriesQuery();
-	const { user } = useAppSelector((state) => state.auth);
+  const [categoryId, setCategoryId] = useState(0);
+  const [subCategoryId, setSubCategoryId] = useState(0);
 
-	const [editItem, { error }] = useEditItemMutation();
+  const { data: categories } = useGetAllcategoriesQuery();
+  const { data: subcategory } = useGetSubcategoriesByIdCategoryQuery(categoryId);
+  const { data: underSubcategory } = useGetUnderSubcategoriesByIdCategoryQuery(subCategoryId);
+  const [openItemId, setOpenItemId] = useState<number | null>(null);
 
-	const [form, setForm] = useState<ItemProps>({
-		item_name: '',
-		item_description: '',
-		id_category: '',
-		id_subcategory: '',
-		id_under_subcategory: '',
-		id_user: '',
-	});
+  const handleClick = (itemId: number) => {
+    setOpenItemId(itemId === openItemId ? null : itemId);
+    setCategoryId(itemId);
+  };
+  const handleClickSubcategory = (itemId: number) => {
+    setOpenItemId(itemId === openItemId ? null : itemId);
+    setSubCategoryId(itemId);
+  };
+  const { user } = useAppSelector((state) => state.auth);
 
-	const [open, setOpen] = useState(false);
+  const [editItem, { error }] = useEditItemMutation();
 
-	const navigate = useNavigate();
+  const [form, setForm] = useState<ItemProps>({
+    item_name: '',
+    item_description: '',
+    id_category: '',
+    id_subcategory: '',
+    id_under_subcategory: '',
+    id_user: '',
+  });
 
-	const handleClose = () => {
-		setOpen(false);
-	};
+  const [open, setOpen] = useState(false);
 
-	useEffect(() => {
-		const fetchData = async () => {
-			await itemById;
-			if (itemById) {
-				setForm((prevState) => ({
-					...prevState,
-					item_name: itemById[0].item_name,
-					item_description: itemById[0].item_description,
-					id_category: itemById[0].id_category,
-					id_subcategory: itemById[0].id_subcategory,
-					id_under_subcategory: itemById[0].id_under_subcategory,
-					id_user: user[0].id,
-				}));
-			}
-		};
+  const navigate = useNavigate();
 
-		fetchData();
-	}, [itemById, user]);
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-	const inputChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-		const { name, value } = e.target;
+  useEffect(() => {
+    const fetchData = async () => {
+      await itemById;
+      if (itemById) {
+        setForm((prevState) => ({
+          ...prevState,
+          item_name: itemById[0].item_name,
+          item_description: itemById[0].item_description,
+          id_category: itemById[0].id_category,
+          id_subcategory: itemById[0].id_subcategory,
+          id_under_subcategory: itemById[0].id_under_subcategory,
+          id_user: user[0].id,
+        }));
+      }
+    };
 
-		setForm((prevState) => ({
-			...prevState,
-			[name as keyof ItemProps]: value,
-		}));
-	};
+    fetchData();
+  }, [itemById, user]);
 
-	const selectChangeHandler = (name: string, value: string) => {
-		setForm((prevState) => ({
-			...prevState,
-			[name as keyof ItemProps]: value,
-		}));
-	};
+  const inputChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
 
-	const submitFormHandler = async (e: FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
+    setForm((prevState) => ({
+      ...prevState,
+      [name as keyof ItemProps]: value,
+    }));
+  };
 
-		if (itemById && form.item_name) {
-			const data = await editItem({ itemId: id as string, item: form });
+  const selectChangeHandler = (name: string, value: string) => {
+    setForm((prevState) => ({
+      ...prevState,
+      [name as keyof ItemProps]: value,
+    }));
+  };
 
-			if (!(data as { error: object }).error) {
-				await refetch();
-				navigate('/');
-			}
-		}
-	};
+  const submitFormHandler = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-	return (
-		<form onSubmit={submitFormHandler}>
-			<Container component="section" maxWidth="xs" sx={{ marginTop: '100px' }}>
-				<Snackbar
-					anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-					open={open}
-					autoHideDuration={3000}
-					onClose={handleClose}
-				>
-					<Alert severity="error" onClose={handleClose}>
-						{(error as CustomError)?.data?.message}
-					</Alert>
-				</Snackbar>
-				<FormElement
-					value={form.item_name}
-					label="Title"
-					name="item_name"
-					onChange={inputChangeHandler}
-				/>
-				<FormElement
-					value={form.item_description}
-					label="Описание"
-					name="item_description"
-					onChange={inputChangeHandler}
-				/>
-				<BasicSelect
-					value={form.id_category}
-					label="Категория"
-					name="id_category"
-					onChange={(value) => selectChangeHandler('id_category', value)}
-					options={
-						categories
-							? categories.map((category) => ({
-									id: category.id,
-									name: category.category_name,
-							  }))
-							: []
-					}
-				/>
-				<FormElement
-					value={form.id_subcategory}
-					label="Подкатегория"
-					name="id_subcategory"
-					onChange={inputChangeHandler}
-				/>
-				<FormElement
-					value={form.id_under_subcategory}
-					label="Подкатегория подкатегории"
-					name="id_under_subcategory"
-					onChange={inputChangeHandler}
-				/>
-				<Button
-					fullWidth
-					variant="contained"
-					color="success"
-					type="submit"
-					className="submit"
-				>
-					Edit
-				</Button>
-			</Container>
-		</form>
-	);
+    if (itemById && form.item_name) {
+      const data = await editItem({ itemId: id as string, item: form });
+
+      if (!(data as { error: object }).error) {
+        await refetch();
+        navigate('/');
+      }
+    }
+  };
+
+  return (
+    <form onSubmit={submitFormHandler}>
+      <Container component="section" maxWidth="xs" sx={{ marginTop: '100px' }}>
+        <Snackbar
+          anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+          open={open}
+          autoHideDuration={3000}
+          onClose={handleClose}
+        >
+          <Alert severity="error" onClose={handleClose}>
+            {(error as CustomError)?.data?.message}
+          </Alert>
+        </Snackbar>
+        <FormElement value={form.item_name} label="Title" name="item_name" onChange={inputChangeHandler} />
+        <FormElement
+          value={form.item_description}
+          label="Описание"
+          name="item_description"
+          onChange={inputChangeHandler}
+        />
+        <BasicSelect
+          value={form.id_category}
+          label="Категория"
+          name="id_category"
+          onChange={(value) => {
+            const categoryId = parseInt(value);
+            selectChangeHandler('id_category', value);
+            handleClick(categoryId);
+          }}
+          options={
+            categories
+              ? categories.map((category) => ({
+                  id: category.id,
+                  name: category.category_name,
+                }))
+              : []
+          }
+        />
+        <BasicSelect
+          value={form.id_subcategory}
+          label="Подкатегория"
+          name="id_subcategory"
+          onChange={(value) => {
+            const subCategoryId = parseInt(value);
+            selectChangeHandler('id_subcategory', value);
+            handleClickSubcategory(subCategoryId);
+          }}
+          options={
+            subcategory
+              ? subcategory.map((subcategory) => ({
+                  id: subcategory.id,
+                  name: subcategory.subcategory_name,
+                }))
+              : []
+          }
+        />
+        <BasicSelect
+          value={form.id_under_subcategory}
+          label="Подкатегория подкатегории"
+          name="id_under_subcategory"
+          onChange={(value) => selectChangeHandler('id_under_subcategory', value)}
+          options={
+            underSubcategory
+              ? underSubcategory.map((undersubs) => ({
+                  id: undersubs.id,
+                  name: undersubs.under_subcategory_name,
+                }))
+              : []
+          }
+        />
+
+        <Button fullWidth variant="contained" color="success" type="submit" className="submit">
+          Edit
+        </Button>
+      </Container>
+    </form>
+  );
 };
 
 export default EditItem;
