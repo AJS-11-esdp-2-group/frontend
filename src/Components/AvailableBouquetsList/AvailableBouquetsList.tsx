17054d8c54ee8f160bbf332be7254764a6efeef0
KenDaryn <https://KenDaryn@github.com>
2023-08-02 17:38:53 +0600

    доработал стилизацию input ввода суммы и скрытие после продажи

--- src/Components/AvailableBouquetsList/AvailableBouquetsList.tsx
+++ src/Components/AvailableBouquetsList/AvailableBouquetsList.tsx
@@ -1,22 +1,31 @@
-import { ListItem, ListItemText, ListItemIcon, Button, TextField, Grid, ThemeProvider, ImageListItem } from '@mui/material';
-import { Clear, Done } from '@mui/icons-material';
-import { GlobalTheme } from '../..';
-import { MouseEventHandler, ChangeEventHandler, useState } from 'react';
+import {
+  ListItem,
+  ListItemText,
+  ListItemIcon,
+  Button,
+  TextField,
+  Grid,
+  ThemeProvider,
+  ImageListItem,
+} from '@mui/material'
+import { Clear, Done } from '@mui/icons-material'
+import { GlobalTheme } from '../..'
+import { MouseEventHandler, ChangeEventHandler, useState } from 'react'
 
 export interface AvailableBouquetsProps {
-  id: number;
-  count: number;
-  name_bouquet: string;
-  actual_price: number;
-  image_bouquet: string;
-  onClick: MouseEventHandler<HTMLDivElement>;
-  isEditing: boolean;
-  editingPrice: number;
-  changePrice: MouseEventHandler<HTMLButtonElement>;
-  handleSaveClick: (itemId: number, newPrice: number) => void;
-  handleCancelClick: () => void;
-  handlePriceChange: ChangeEventHandler<HTMLInputElement>;
-  sellBouquet: (id: number, totalSum: number, qty: number) => void;
+  id: number
+  count: number
+  name_bouquet: string
+  actual_price: number
+  image_bouquet: string
+  onClick: MouseEventHandler<HTMLDivElement>
+  isEditing: boolean
+  editingPrice: number
+  changePrice: MouseEventHandler<HTMLButtonElement>
+  handleSaveClick: (itemId: number, newPrice: number) => void
+  handleCancelClick: () => void
+  handlePriceChange: ChangeEventHandler<HTMLInputElement>
+  sellBouquet: (id: number, totalSum: number, qty: number) => void
 }
 
 const AvailableBouquetsList = ({
@@ -32,31 +41,35 @@ const AvailableBouquetsList = ({
   handleCancelClick,
   sellBouquet,
 }: AvailableBouquetsProps) => {
-  const [totalSum, setTotalSum] = useState(actual_price);
-  const [previousTotalSum, setPreviousTotalSum] = useState(actual_price);
-  const [qty, setQty] = useState(1);
+  const [totalSum, setTotalSum] = useState(actual_price)
+  const [previousTotalSum, setPreviousTotalSum] = useState(actual_price)
+  const [qty, setQty] = useState(1)
 
-  const handlePriceInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setTotalSum(Number(event.target.value));
-  };
+  const handlePriceInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    setTotalSum(Number(event.target.value))
+  }
 
   const handleSaveButtonClick = () => {
-    handleSaveClick(id, totalSum || actual_price);
-    setPreviousTotalSum(totalSum);
-  };
+    handleSaveClick(id, totalSum || actual_price)
+    setPreviousTotalSum(totalSum)
+  }
 
   const handleCancelClickInternal = () => {
-    setTotalSum(previousTotalSum);
-    handleCancelClick();
-  };
+    setTotalSum(previousTotalSum)
+    handleCancelClick()
+  }
 
   const handleQtyChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setQty(Number(event.target.value));
-  };
+    setQty(Number(event.target.value))
+  }
 
   const handleSellButtonClick = () => {
-    sellBouquet(id, totalSum || actual_price, qty); 
-  };
+    sellBouquet(id, totalSum || actual_price, qty)
+    setTotalSum(previousTotalSum)
+    handleCancelClick()
+  }
 
   return (
     <>
@@ -64,8 +77,11 @@ const AvailableBouquetsList = ({
         <ImageListItem
           key={id}
           style={{
-            border: '1px solid #ccc', borderRadius: '5px', padding: '5px'
-          }}>
+            border: '1px solid #ccc',
+            borderRadius: '5px',
+            padding: '5px',
+          }}
+        >
           <div onClick={onClick}>
             <img
               src={image_bouquet}
@@ -81,13 +97,17 @@ const AvailableBouquetsList = ({
             />
             <ListItem>
               <ListItemText primary={name_bouquet} />
-              <ListItemText secondary={'Количество букетов в наличии: ' + count} />
-              <ListItemText secondary={'Цена букета по прайсу ' + actual_price + ' тенге'} />
+              <ListItemText
+                secondary={'Количество букетов в наличии: ' + count}
+              />
+              <ListItemText
+                secondary={'Цена букета по прайсу ' + actual_price + ' тенге'}
+              />
               <ListItemText secondary={'Цена продажи ' + totalSum + ' тенге'} />
             </ListItem>
           </div>
           {isEditing ? (
-            <Grid>
+            <Grid sx={{ pb: 3, pt: 3 }}>
               <TextField
                 value={totalSum}
                 id="outlined-basic"
@@ -118,11 +138,10 @@ const AvailableBouquetsList = ({
         </ImageListItem>
       </ThemeProvider>
     </>
-  );
-};
-
-export default AvailableBouquetsList;
-function qty(id: number, arg1: number, qty: any) {
-  throw new Error('Function not implemented.');
+  )
 }
 
+export default AvailableBouquetsList
+function qty(id: number, arg1: number, qty: any) {
+  throw new Error('Function not implemented.')
+}
