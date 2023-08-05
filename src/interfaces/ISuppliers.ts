536f8ba21fa92a75da0eb4f071fe30a0864bf075
Moon Natalia <nataliya.moon@gmail.com>
2023-08-05 16:22:22 +0600

    убрала страны и города из поставщиков, добавила комментарий

--- src/interfaces/ISuppliers.ts
+++ src/interfaces/ISuppliers.ts
@@ -1,16 +1,11 @@
 export interface ISuppliers extends ISupplier {
 	id: number;
-	create_date: Date;
-	name_country: string;
-	name_city: string;
 }
 
 export interface ISupplier {
 	name_supplier: string;
-	contact_person: string;
 	email: string;
 	phone: string;
 	address: string;
-	id_country: string;
-	id_city: string;
+	comment: string;
 }
