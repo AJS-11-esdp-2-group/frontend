746d67d31c1db9f828fab3acabbfd90a182994b6
KenDaryn <https://KenDaryn@github.com>
2023-08-08 21:14:52 +0600

    добавил отправку букета на витрину

--- src/Container/AvailableBouquets/AvailableBouquets.tsx
+++ src/Container/AvailableBouquets/AvailableBouquets.tsx
@@ -1,140 +1,163 @@
 /* eslint-disable @typescript-eslint/await-thenable */
-import { useEditAvailableBouquetMutation, 
-    useGetAllAvailableBouquetsQuery, 
-    useGetAvailableBouquetByIdQuery,
-} from '../../Store/services/availableBouquets';
-import { IAvailableBouquets } from '../../interfaces/IAvailableBouquets';
-import AvailableBouquetsList from '../../Components/AvailableBouquetsList/AvailableBouquetsList';
-import Loading from '../../Components/UI/Loading/Loading';
-import { useEffect, useState } from 'react';
 import {
-    Box,
-    Button,
-    Container,
-    Dialog,
-    DialogActions,
-    DialogContent,
-    DialogContentText,
-    DialogTitle,
-    Grid,
-    List,
-    ListSubheader,
-} from '@mui/material';
+  useEditAvailableBouquetMutation,
+  useGetAllAvailableBouquetsQuery,
+  useGetAvailableBouquetByIdQuery,
+} from '../../Store/services/availableBouquets'
+import { IAvailableBouquets } from '../../interfaces/IAvailableBouquets'
+import AvailableBouquetsList from '../../Components/AvailableBouquetsList/AvailableBouquetsList'
+import Loading from '../../Components/UI/Loading/Loading'
+import { useEffect, useState } from 'react'
+import {
+  Box,
+  Button,
+  Container,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Grid,
+  List,
+  ListSubheader,
+} from '@mui/material'
 
 const AvailableBouquets = () => {
-    const { data, isLoading, isError, error } = useGetAllAvailableBouquetsQuery();
-    const [open, setOpen] = useState(false);
-    const [openModal, setOpenModal] = useState(false);
-    const [bouquets, setBouquets] = useState<IAvailableBouquets[]>([]);
-    const [editingPriceId, setEditingPriceId] = useState<number | null>(null);
-    const [editingPrice, setEditingPrice] = useState(0);
-    const [selectedBouquetId, setSelectedBouquetId] = useState<number>(0);
-    const { data: items } = useGetAvailableBouquetByIdQuery(selectedBouquetId);
-    const [changePrice] = useEditAvailableBouquetMutation();
+  const { data, isLoading, isError, error } = useGetAllAvailableBouquetsQuery()
+  const [open, setOpen] = useState(false)
+  const [openModal, setOpenModal] = useState(false)
+  const [bouquets, setBouquets] = useState<IAvailableBouquets[]>([])
+  const [editingPriceId, setEditingPriceId] = useState<number | null>(null)
+  const [editingPrice, setEditingPrice] = useState(0)
+  const [selectedBouquetId, setSelectedBouquetId] = useState<number>(0)
+  const { data: items, refetch } = useGetAvailableBouquetByIdQuery(
+    selectedBouquetId,
+  )
+  const [changePrice] = useEditAvailableBouquetMutation()
 
-    useEffect(() => {
-        setOpen(isError);
-    }, [isError]);
+  useEffect(() => {
+    refetch()
+  }, [refetch])
 
-    useEffect(() => {
-        if (data) {
-            setBouquets(data as []);
-        }
-    }, [data]);
+  useEffect(() => {
+    setOpen(isError)
+  }, [isError])
 
-    const onClick = (id: number) => {
-        setSelectedBouquetId(id);
-        setOpen(true);
-    };
+  useEffect(() => {
+    if (data) {
+      setBouquets(data as [])
+    }
+  }, [data])
 
-    const handleClose = () => {
-        setOpen(false);
-        setOpenModal(false);
-    };
+  const onClick = (id: number) => {
+    setSelectedBouquetId(id)
+    setOpen(true)
+  }
 
-    const handleEditPrice = (bouquetId: number, total_sum: number) => {
-        setEditingPrice(total_sum);
-        setEditingPriceId(bouquetId);
-    };
+  const handleClose = () => {
+    setOpen(false)
+    setOpenModal(false)
+  }
 
-    const handlePriceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setEditingPrice(Number(event.target.value));
-    };
+  const handleEditPrice = (bouquetId: number, total_sum: number) => {
+    setEditingPrice(total_sum)
+    setEditingPriceId(bouquetId)
+  }
 
-    const handleSaveClick = (id: number, newSum: number, newCount: number) => {
-        const newPrice = newSum || editingPrice;
-        const updatedBouquets = bouquets.map((bouquet) => {
-            if (id === bouquet.id) {
-                return { ...bouquet, total_sum: newPrice, count: newCount };
-            }
-            return bouquet;
-        });
+  const handlePriceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEditingPrice(Number(event.target.value))
+  }
 
-        setBouquets(updatedBouquets);
-        setEditingPriceId(null);
-    };
+  const handleSaveClick = (id: number, newSum: number, newCount: number) => {
+    const newPrice = newSum || editingPrice
+    const updatedBouquets = bouquets.map((bouquet) => {
+      if (id === bouquet.id) {
+        return { ...bouquet, total_sum: newPrice, count: newCount }
+      }
+      return bouquet
+    })
 
-    const sellBouquet = async (id: number, totalSum: number, qty: number) => {
-        await changePrice({
-            id: id,
-            bouquet: { total_sum: totalSum, count: qty },
-        });
-    };
+    setBouquets(updatedBouquets)
+    setEditingPriceId(null)
+  }
 
-    return (
-        <Container style={{
-            display: 'flex',
-            justifyContent: 'center',
-            flexDirection: 'column',
-        }}>
-                {isLoading && <Loading/>}
-            <Grid container sx={{ maxWidth: '400px', margin: '0 auto', my: 2 }}>
-                <Grid item xs={12}>
-                    <Box>
-                        <ListSubheader component="div">Букеты в наличии</ListSubheader>
-                        {bouquets &&
-                            bouquets.map((bouquet) => {
-                                return (
-                                    <Grid item key={bouquet.id}>
-                                        <List>
-                                            <AvailableBouquetsList
-                                                id={bouquet.id}
-                                                count={bouquet.count}
-                                                name_bouquet={bouquet.name_bouquet}
-                                                image_bouquet={bouquet.image_bouquet}
-                                                actual_price={bouquet.actual_price}
-                                                onClick={() => onClick(bouquet.id)}
-                                                changePrice={() => handleEditPrice(bouquet.id, bouquet.actual_price)}
-                                                handleCancelClick={() => setEditingPriceId(null)}
-                                                isEditing={editingPriceId === bouquet.id}
-                                                handleSaveClick={(bouquetId, newPrice) => handleSaveClick(bouquetId, newPrice, bouquet.count)}
-                                                editingPrice={editingPrice}
-                                                handlePriceChange={handlePriceChange}
-                                                sellBouquet={(id, totalSum, qty) => sellBouquet(id, totalSum, qty)}
-                                            />
-                                        </List>
-                                    </Grid>
-                                );
-                            })}
-                    </Box>
-                </Grid>
-            </Grid>
-            <Dialog open={open} onClose={handleClose} PaperProps={{ style: { backgroundColor: '#F0F0F0' } }}>
-                <DialogTitle >Состав букета</DialogTitle>
-                <DialogContent>
-                    {items &&
-                        items.map((item) => (
-                            <DialogContentText key={item.id} style={{ color: 'black' }}>
-                                {item.item_name}: {item.qty} штук(-а) по {item.price} тенге
-                            </DialogContentText>
-                        ))}
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose}>Закрыть</Button>
-                </DialogActions>
-            </Dialog>
-        </Container>
-    );
-};
+  const sellBouquet = async (id: number, totalSum: number, qty: number) => {
+    await changePrice({
+      id: id,
+      bouquet: { total_sum: totalSum, count: qty },
+    })
+  }
 
-export default AvailableBouquets;
+  return (
+    <Container
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'column',
+      }}
+    >
+      {isLoading && <Loading />}
+      <Grid container sx={{ maxWidth: '400px', margin: '0 auto', my: 2 }}>
+        <Grid item xs={12}>
+          <Box>
+            <ListSubheader component="div">Букеты в наличии</ListSubheader>
+            {bouquets &&
+              bouquets.map((bouquet) => {
+                return (
+                  <Grid item key={bouquet.id}>
+                    <List>
+                      <AvailableBouquetsList
+                        id={bouquet.id}
+                        count={bouquet.count}
+                        name_bouquet={bouquet.name_bouquet}
+                        image_bouquet={
+                          bouquet.image_bouquet
+                            ? bouquet.image_bouquet
+                            : '../no-photo.png'
+                        }
+                        actual_price={bouquet.actual_price}
+                        onClick={() => onClick(bouquet.id)}
+                        changePrice={() =>
+                          handleEditPrice(bouquet.id, bouquet.actual_price)
+                        }
+                        handleCancelClick={() => setEditingPriceId(null)}
+                        isEditing={editingPriceId === bouquet.id}
+                        handleSaveClick={(bouquetId, newPrice) =>
+                          handleSaveClick(bouquetId, newPrice, bouquet.count)
+                        }
+                        editingPrice={editingPrice}
+                        handlePriceChange={handlePriceChange}
+                        sellBouquet={(id, totalSum, qty) =>
+                          sellBouquet(id, totalSum, qty)
+                        }
+                      />
+                    </List>
+                  </Grid>
+                )
+              })}
+          </Box>
+        </Grid>
+      </Grid>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        PaperProps={{ style: { backgroundColor: '#F0F0F0' } }}
+      >
+        <DialogTitle>Состав букета</DialogTitle>
+        <DialogContent>
+          {items &&
+            items.map((item) => (
+              <DialogContentText key={item.id} style={{ color: 'black' }}>
+                {item.item_name}: {item.qty} штук(-а) по {item.price} тенге
+              </DialogContentText>
+            ))}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Закрыть</Button>
+        </DialogActions>
+      </Dialog>
+    </Container>
+  )
+}
+
+export default AvailableBouquets
