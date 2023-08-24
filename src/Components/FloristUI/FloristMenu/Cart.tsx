73eda7029abd6fc2ee8e29a1c6eec57a6e508156
Баилова Таншолпан <bailovat1@gmail.com>
2023-08-24 23:02:30 +0600

    вернула изменения

--- src/Components/FloristUI/FloristMenu/Cart.tsx
+++ src/Components/FloristUI/FloristMenu/Cart.tsx
@@ -38,7 +38,7 @@ const Cart = ({
             <Grid container>
                 <Grid>
                     <Typography color='primary' variant='h6'>
-                        {items?.length > 0 && bouquets?.length === 0 ? 'Ассортимент' : 'Корзина пуста'}
+                        {items?.length > 0 && bouquets?.length === 0 ? 'Ассортимент' : null}
                         {bouquets?.length > 0 && items?.length === 0 ? 'Букеты' : null}
                     </Typography>
                 </Grid>
@@ -61,7 +61,7 @@ const Cart = ({
                                       );
                                   })
                                 : null}
-                            {items?.length  >= 1 &&
+                            {items?.length  >= 0 &&
                                 bouquets?.length &&
                                 bouquets?.map((bouquet, i) => {
                                     return (
